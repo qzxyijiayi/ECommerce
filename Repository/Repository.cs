c6fca6f600e2d41cf8eaa31065bf4de512707065
覃振兴 <1304302416@qq.com>
2019-07-18 21:27:19 +0800

    仓储实现修改

--- Repository/Repository.cs
+++ Repository/Repository.cs
@@ -27,77 +27,111 @@ namespace Repository
             }
         }
 
-        public string TableName { private get; set; } = typeof(TAggregationRoot).Name;
+        public string TableName { get; set; } = typeof(TAggregationRoot).Name;
 
-        public IDbTransaction Transaction { private get; set; } = null;
 
         public virtual int Add(TAggregationRoot aggregationRoot)
         {
-            var sql = "Insert Into{0} Values({1})";
-            if (_sqlSnippets == null) _sqlSnippets = aggregationRoot.GetType().GetProperties().Select(pro => $"{pro.Name} = @{pro.GetValue(aggregationRoot)}");
-            var sqlSb = new StringBuilder();
-            sqlSb = sqlSb.AppendJoin(',', _sqlSnippets);
-            return Connection.Execute(string.Format(sql, TableName, sqlSb.ToString()), aggregationRoot, Transaction);
+            using (Connection)
+            {
+                var sql = "Insert Into{0} Values({1})";
+                if (_sqlSnippets == null) _sqlSnippets = aggregationRoot.GetType().GetProperties().Select(pro => $"{pro.Name} = @{pro.GetValue(aggregationRoot)}");
+                var sqlSb = new StringBuilder();
+                sqlSb = sqlSb.AppendJoin(',', _sqlSnippets);
+                return Connection.Execute(string.Format(sql, TableName, sqlSb.ToString()), aggregationRoot);
+            }
         }
 
         public virtual int Add(IList<TAggregationRoot> aggregationRoots)
         {
-            var i = 0;
-            foreach (var aggregationRoot in aggregationRoots)
+            using (Connection)
             {
-                i += Add(aggregationRoot);
+                var i = 0;
+                foreach (var aggregationRoot in aggregationRoots)
+                {
+                    var sql = "Insert Into{0} Values({1})";
+                    if (_sqlSnippets == null) _sqlSnippets = aggregationRoot.GetType().GetProperties().Select(pro => $"{pro.Name} = @{pro.GetValue(aggregationRoot)}");
+                    var sqlSb = new StringBuilder();
+                    sqlSb = sqlSb.AppendJoin(',', _sqlSnippets);
+                    i += Connection.Execute(string.Format(sql, TableName, sqlSb.ToString()), aggregationRoot);
+                }
+                return i;
             }
-            return i;
         }
 
         public virtual async Task<int> AddAscyn(TAggregationRoot aggregationRoot)
         {
-            var sql = "Insert Into{0} Values({1})";
-            if (_sqlSnippets == null) _sqlSnippets = aggregationRoot.GetType().GetProperties().Select(pro => $"{pro.Name} = @{pro.GetValue(aggregationRoot)}");
-            var sqlSb = new StringBuilder();
-            sqlSb = sqlSb.AppendJoin(',', _sqlSnippets);
-            return await Connection.ExecuteAsync(string.Format(sql, TableName, sqlSb.ToString()), aggregationRoot, Transaction);
+            using (Connection)
+            {
+                var sql = "Insert Into{0} Values({1})";
+                if (_sqlSnippets == null) _sqlSnippets = aggregationRoot.GetType().GetProperties().Select(pro => $"{pro.Name} = @{pro.GetValue(aggregationRoot)}");
+                var sqlSb = new StringBuilder();
+                sqlSb = sqlSb.AppendJoin(',', _sqlSnippets);
+                return await Connection.ExecuteAsync(string.Format(sql, TableName, sqlSb.ToString()), aggregationRoot);
+            }
         }
         public virtual async Task<int> AddAscyn(IList<TAggregationRoot> aggregationRoots)
         {
-            var i = 0;
-            foreach (var aggregationRoot in aggregationRoots)
+            using (Connection)
             {
-                i += await AddAscyn(aggregationRoot);
+                var i = 0;
+                foreach (var aggregationRoot in aggregationRoots)
+                {
+                    var sql = "Insert Into{0} Values({1})";
+                    if (_sqlSnippets == null) _sqlSnippets = aggregationRoot.GetType().GetProperties().Select(pro => $"{pro.Name} = @{pro.GetValue(aggregationRoot)}");
+                    var sqlSb = new StringBuilder();
+                    sqlSb = sqlSb.AppendJoin(',', _sqlSnippets);
+                    i += await Connection.ExecuteAsync(string.Format(sql, TableName, sqlSb.ToString()), aggregationRoot);
+                }
+                return i;
             }
-            return i;
         }
 
         public virtual TAggregationRoot Query(TAggregationRootId aggregationRootId)
         {
-            var sql = $"SELECT * FROM {TableName} Where Id = @Id";
-            return Connection.QueryFirstOrDefault<TAggregationRoot>(sql, new { Id = aggregationRootId }, Transaction);
+            using (Connection)
+            {
+                var sql = $"SELECT * FROM {TableName} Where Id = @Id";
+                return Connection.QueryFirstOrDefault<TAggregationRoot>(sql, new { Id = aggregationRootId });
+            }
         }
 
         public virtual IEnumerable<TAggregationRoot> Query(IList<TAggregationRootId> aggregationRootIds)
         {
-            var sql = $"SELECT * FROM {typeof(TAggregationRoot).Name} Where Id in ({string.Join(',', aggregationRootIds)})";
-            return Connection.Query<TAggregationRoot>(sql, null, Transaction);
+            using (Connection)
+            {
+                var sql = $"SELECT * FROM {typeof(TAggregationRoot).Name} Where Id in ({string.Join(',', aggregationRootIds)})";
+                return Connection.Query<TAggregationRoot>(sql, null);
+            }
         }
 
         public virtual async Task<IEnumerable<TAggregationRoot>> QueryAscyn(IList<TAggregationRootId> aggregationRootIds)
         {
-            var sql = $"SELECT * FROM {typeof(TAggregationRoot).Name} Where Id in ({string.Join(',', aggregationRootIds)})";
-            return await Connection.QueryAsync<TAggregationRoot>(sql, null, Transaction);
+            using (Connection)
+            {
+                var sql = $"SELECT * FROM {typeof(TAggregationRoot).Name} Where Id in ({string.Join(',', aggregationRootIds)})";
+                return await Connection.QueryAsync<TAggregationRoot>(sql, null);
+            }
         }
 
         public virtual async Task<TAggregationRoot> QueryAsync(TAggregationRootId aggregationRootId)
         {
-            var sql = $"SELECT * FROM {typeof(TAggregationRoot).Name} Where Id = @Id";
-            return await Connection.QueryFirstOrDefaultAsync<TAggregationRoot>(sql, new { Id = aggregationRootId }, Transaction);
+            using (Connection)
+            {
+                var sql = $"SELECT * FROM {typeof(TAggregationRoot).Name} Where Id = @Id";
+                return await Connection.QueryFirstOrDefaultAsync<TAggregationRoot>(sql, new { Id = aggregationRootId });
+            }
         }
 
 
 
         public virtual int Remove(TAggregationRoot aggregationRoot)
         {
-            var sql = $"Delete FROM {TableName} Where Id = @Id";
-            return Connection.Execute(sql, new { Id = aggregationRoot.AggregationRootId }, Transaction);
+            using (Connection)
+            {
+                var sql = $"Delete FROM {TableName} Where Id = @Id";
+                return Connection.Execute(sql, new { Id = aggregationRoot.AggregationRootId });
+            }
         }
 
         public virtual int Remove(IList<TAggregationRoot> aggregationRoots)
@@ -115,7 +149,7 @@ namespace Repository
         public virtual async Task<int> RemoveAscyn(TAggregationRoot aggregationRoot)
         {
             var sql = $"Delete FROM {TableName} Where Id = @Id";
-            return await Connection.ExecuteAsync(sql, new { Id = aggregationRoot.AggregationRootId }, Transaction);
+            return await Connection.ExecuteAsync(sql, new { Id = aggregationRoot.AggregationRootId });
         }
 
         public virtual async Task<int> RemoveAscyn(IList<TAggregationRoot> aggregationRoots)
